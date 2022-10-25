fd017c27981a2bf8237b966ff44c8b1001326682
mangofeet <purpleplasticsloth@gmail.com>
2022-10-25 10:18:27 -0600

    add service worker

--- main.js
+++ main.js
@@ -282,3 +282,27 @@ addListener('selectPreset', 'change', handlePresetChange)
 handlePreset.apply(null, defaultPresets["LRM 20"])
 renderPresets()
 document.getElementById("inputRollCount").value = 1
+
+const registerServiceWorker = async () => {
+  try {
+    const registration = await navigator.serviceWorker.register(
+      './sw.js',
+      {
+        scope: '/',
+      }
+    );
+    if (registration.installing) {
+      console.log('Service worker installing');
+    } else if (registration.waiting) {
+      console.log('Service worker installed');
+    } else if (registration.active) {
+      console.log('Service worker active');
+    }
+  } catch (error) {
+    console.error(`Registration failed with ${error}`);
+  }
+}
+
+registerServiceWorker()
+
+
---
+++ sw.js
@@ -0,0 +1,93 @@
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open('v1');
+  await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to use the preloaded response, if it's there
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    console.info('using preload response', preloadResponse);
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+};
+
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    // Enable navigation preloads!
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      '/',
+      '/index.html',
+      '/simple.css',
+      '/style.css',
+      '/main.js',
+      '/presets.js',
+      '/img/d6-1.svg',
+      '/img/d6-2.svg',
+      '/img/d6-3.svg',
+      '/img/d6-4.svg',
+      '/img/d6-5.svg',
+      '/img/d6-6.svg',
+      '/img/icon/android-chrome-192x192.png',
+      '/img/icon/android-chrome-512x512.png',
+      '/img/icon/apple-touch-icon.png',
+      '/img/icon/favicon-16x16.png',
+      '/img/icon/favicon-32x32.png',
+      '/img/icon/favicon.ico',
+    ])
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
+      fallbackUrl: '/img/icon/apple-touch-icon.png',
+    })
+  );
+});
